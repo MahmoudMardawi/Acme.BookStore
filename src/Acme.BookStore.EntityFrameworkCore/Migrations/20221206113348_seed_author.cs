c7c83d87b3671cfce859f116f876e0eff3c1a53b
MahmoudMardawi <ma.mardawi@gmail.com>
2022-12-06 13:38:46 +0200

    Create the Applicatoin Layer

---
+++ src/Acme.BookStore.EntityFrameworkCore/Migrations/20221206113348_seed_author.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class seed_author : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors");
+
+            migrationBuilder.RenameTable(
+                name: "Authors",
+                newName: "AppAuthors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AppAuthors",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppAuthors",
+                table: "AppAuthors",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppAuthors_Name",
+                table: "AppAuthors",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppAuthors",
+                table: "AppAuthors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppAuthors_Name",
+                table: "AppAuthors");
+
+            migrationBuilder.RenameTable(
+                name: "AppAuthors",
+                newName: "Authors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Authors",
+                table: "Authors",
+                column: "Id");
+        }
+    }
+}
